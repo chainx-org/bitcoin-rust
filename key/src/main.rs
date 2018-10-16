bd40a6a46cdb5c6cf4046fdd1f28a5928caf72e2
icodezjb <icodezjb@163.com>
2018-10-16 15:41:30 +0800

    Output compressed public key (#15)

--- key/src/main.rs
+++ key/src/main.rs
@@ -1,10 +1,19 @@
 extern crate keys;
 
 use keys::generator::Generator;
+use keys::{Private, KeyPair};
 
 fn main() {
     let random = keys::generator::Random::new(keys::Network::Testnet);
     let key = random.generate().unwrap();
     println!("{:?}", key);
+    let private = key.private();
+    let compressed_private = Private{
+        network: private.network.clone(),
+        secret: private.secret.clone(),
+        compressed: true,
+    };
+    let compressed = KeyPair::from_private(compressed_private).unwrap();
+    println!("compressed public key: {}",compressed.public());
     println!("address:{:?}", key.address().to_string());
 }
